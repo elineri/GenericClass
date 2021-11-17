65f316cd8debb3bcc4fb0bd124af5e0d5d8ed585
elineri <elin.ericstam@gmail.com>
2021-11-17 15:58:18 +0100

    Skapat en generic klass Check och en metod ToCheck som jämför två variabler.

---
+++ GenericClass/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClass
+{
+    class Check<Unknown>
+    {
+        public void ToCheck(Unknown var1, Unknown var2)
+        {
+            var1.Equals(var2);
+        }
+    }
+}
